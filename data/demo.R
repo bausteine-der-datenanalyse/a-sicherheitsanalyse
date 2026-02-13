f2ec16a4bbf9416c99530fcb938b97347dd27110
FrancaHollmann <franca.hollmann@hs-bochum.de>
2026-02-13 21:33:05 +0100

    Updates

--- data/demo.R
+++
@@ -1,9 +0,0 @@
-library(fs)
-library(curl)
-library(tidyverse)
-
-if (!file_exists("data/BFStr_Netz_v2025q3.gpkg")) {
-  curl_download("https://www.bast.de/SharedDocs/Daten-TB/Daten-BISStra.zip?__blob=publicationFile&v=5", destfile = "data/", quiet = FALSE)
-  unzip("data/Daten-BISStra.zip", exdir = "data")
-}
-
